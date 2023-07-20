e6fa708ce003aa66e34dc8d5a82c5915561249c8
AitorParr <115299335+AitorParr@users.noreply.github.com>
2023-07-19 23:12:04 -0400

    Update DataFrame-Management.r

--- DataFrame-Management.r
+++ DataFrame-Management.r
@@ -5,6 +5,7 @@ library(tidyverse)
 library(ggplot2)
 library(scales)
 library(readxl)
+library(openxlsx)
 library(zoo)
 
 # Dataframe init ----------------------------------------------------------
@@ -33,6 +34,9 @@ coin_Uniswap <- read_csv("Dataframes/coin_Uniswap.csv")
 coin_WrappedBitcoin <- read_csv("Dataframes/coin_WrappedBitcoin.csv")
 coin_XRP <- read_csv("Dataframes/coin_XRP.csv")
 MES_0423 <- read_csv("Dataframes/MES_0423.csv")
+BTC_Footprints_v1 <- read_excel("Dataframes/BTC_Footprints_v1.xlsx")
+owid_co2_data <- read_excel("Dataframes/owid-co2-data.xlsx")
+
 
 # Dataframe Cleaning ----------------------------------------------------------
 
@@ -88,6 +92,42 @@ power_df <- power_df %>%
             filter(!(Country %in% c("IEA Total","OECD Americas","OECD Asia Oceania","OECD Europe","OECD Total"))) %>% 
             filter(Product == "Total Combustible Fuels")
 
+
+##DIOS MIO QUE DOLOR DE CABEZA LIMPIAR ESTO 
+
+power_bt_df <- BTC_Footprints_v1
+
+##Algunas de las fechas del dataframe se transforman en numeros porque excel es especial
+##Si todas se transformaran no seria un problema pero en el df original las fechas del 1 al 12 estan en
+##otro formato (ni idea), Asi que las separo en las fechas normales y las numero
+power_bt_df_g <- dplyr::filter(power_bt_df, grepl('/', Date))
+power_bt_df_w <- dplyr::filter(power_bt_df, !grepl('/', Date))
+
+##Le digo a R que deje de tratar esas fechas como fechas y que las vuelva numero
+power_bt_df_w$Date <- as.numeric(power_bt_df_w$Date)
+##Ahora los vulevo fechas en base a la fecha de excel
+power_bt_df_w$Date <- as.Date(power_bt_df_w$Date,origin = "1899-12-30")
+
+
+##Estas ya estaban bien, solo las vuelvo year month
+power_bt_df_g <- power_bt_df_g %>% mutate(Date=as.yearmon(Date,"%m/%d/%Y"))
+
+##Las vuelvo char para que R se olvide del formato original en la que las tenia
+##Las vuelvo fecha otra vez pero con distinto formato
+##Las vuelvo yearmon (No se porque funciona, pero no funciono los otros metodos que busque)
+power_bt_df_w <- power_bt_df_w %>%
+  mutate(Date = Date %>% 
+           str_replace_all("-", "/"))
+power_bt_df_w <- power_bt_df_w %>% mutate(Date=as.character(Date))
+power_bt_df_w <- power_bt_df_w %>% mutate(Date=as.Date(Date,"%Y/%d/%m"))
+power_bt_df_w <- power_bt_df_w %>% mutate(Date=as.yearmon(Date))
+
+##Las fechas raras (Weird) + las fechas buenas (Good) = dataframe original
+power_bt_df <- rbind(power_bt_df_g,power_bt_df_w)
+
+
+##Le quito las lineas en blanco, le doy nombre a las columnas, quito aquellas cosas que no sean paises
+##formateo la fecha para que sea yearmon
 power_df_total <- MES_0423 
 names(power_df_total) <- power_df_total[8,]
 colnames(power_df_total)[5] <- "Amount"
@@ -109,6 +149,10 @@ power_df_total <- power_df_total %>%
   group_by(Time,Product,Unit) %>% 
   summarize(Avg_amount = sum(Amount))
 
+power_bt_df_summary <- power_bt_df %>% 
+  group_by(Date) %>%
+  summarize(Avg_Energy = (mean(BTCENEGUE)))
+
 value_df_summary <- coins %>% 
   group_by(Date) %>%
   summarize(Avg_Value = (mean(High)+mean(Low))/2)
@@ -116,6 +160,8 @@ value_df_summary <- coins %>%
 volumen_df_summary <- coins %>% 
   group_by(Date)  %>%
   summarize(Volume = mean(Volume))
+
+power_bt_power_total_df <- left_join(power_df_total,power_bt_df_summary,by=c("Time"="Date")) 
 
 power_value_df <- left_join(value_df_summary,power_df,by=c("Date"="Time"))
 
@@ -159,6 +205,14 @@ power <- power_df %>%
   labs(y="Energia Generada por Combustibles", x="Años")+
   scale_x_date(date_breaks = "1 year",date_labels = "%Y")+
   ggtitle("Energia creada con fuentes combustibles por año")+
+  theme(axis.title=element_text(size=10,face="bold"),axis.text.x = element_text(size = 8,angle = 60))
+
+power_bt <- power_bt_df_summary %>%  
+  ggplot() +
+  geom_col(aes(x = as.Date(Date,format = "%y-%m-%d"), y = Avg_Energy),color="black",fill="purple") + 
+  labs(y="Energia Consumida por BT", x="Años")+
+  scale_x_date(date_breaks = "1 year",date_labels = "%Y")+
+  ggtitle("Energia consumida por BT")+
   theme(axis.title=element_text(size=10,face="bold"),axis.text.x = element_text(size = 8,angle = 60))
 
 power_total <- power_df_total %>%  
@@ -208,6 +262,14 @@ power_total_volume <-power_total_volume_df %>%
               formula = y ~ x,
               geom = "smooth")+
   theme(axis.title=element_text(size=10,face="bold"),axis.text.x = element_text(size = 8,angle = 60))
+
+
+
+power_bt_power <-power_bt_power_total_df  %>%  
+  ggplot() +
+  geom_line(aes(x = as.Date(Time,format = "%y-%m-%d"), y = Avg_amount),color="magenta") +
+  geom_line(aes(x = as.Date(Time,format = "%y-%m-%d"), y = Avg_Energy),color="purple") 
+  
 
 #Correlation---------------------------------------------------------
 
